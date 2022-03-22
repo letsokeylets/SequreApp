0365610ee564cde33f7963c0e91c89e406d7bd08
Mozdir.Taras <letsokey@mail.ru>
2022-03-22 17:53:12 +0400

    Autorized system created

--- src/main/java/netology/ru/sequreapp/repository/HibernateRepository.java
+++ src/main/java/netology/ru/sequreapp/repository/HibernateRepository.java
@@ -3,10 +3,12 @@ package netology.ru.sequreapp.repository;
 import netology.ru.sequreapp.entity.InfoPerson;
 import netology.ru.sequreapp.entity.Persons;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface HibernateRepository extends JpaRepository<Persons, InfoPerson> {
 
     List<Persons> findByCityOfLiving(String city);
